1af3bda6ebee9260f157ae648cd8febc8015c515
Pedro Pereira <pedro.2108@hotmail.com>
2024-05-22 22:13:18 -0300

    feat: content selector implemented

---
+++ src/components/ContentSelector.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+export function ContentSelector() {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  return (
+    <section className="flex space-x-2">
+      <Link href="/" passHref>
+        <Button
+          size="sm"
+          variant={pathname === '/' ? 'default' : 'ghost'}
+          className={clsx(
+            'text-sm',
+            pathname !== '/' && 'text-muted-foreground'
+          )}
+        >
+          About
+        </Button>
+      </Link>
+      <Link href="/posts" passHref>
+        <Button
+          size="sm"
+          variant={pathname === '/posts' ? 'default' : 'ghost'}
+          className={clsx(
+            'text-sm',
+            pathname !== '/posts' && 'text-muted-foreground'
+          )}
+        >
+          Posts
+        </Button>
+      </Link>
+      <Link href="/newsletter" passHref>
+        <Button
+          size="sm"
+          variant={pathname === '/newsletter' ? 'default' : 'ghost'}
+          className={clsx(
+            'text-sm',
+            pathname !== '/newsletter' && 'text-muted-foreground'
+          )}
+        >
+          Newsletter
+        </Button>
+      </Link>
+      <Link href="/contact" passHref>
+        <Button
+          size="sm"
+          variant={pathname === '/contact' ? 'default' : 'ghost'}
+          className={clsx(
+            'text-sm',
+            pathname !== '/contact' && 'text-muted-foreground'
+          )}
+        >
+          Contact me
+        </Button>
+      </Link>
+    </section>
+  );
+}
