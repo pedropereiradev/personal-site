8f6cad5029e6c77ca84130a9e921deaca8e76c10
Pedro Pereira <pedro.2108@hotmail.com>
2024-05-22 22:11:23 -0300

    feat: initial newsletter page implemented

---
+++ src/app/newsletter/page.tsx
@@ -0,0 +1,12 @@
+export default function Newsletter() {
+  return (
+    <main className="max-w-5xl mx-auto my-auto mt-4">
+      <section className="flex justify-center">
+        <p className="text-lg text-muted-foreground max-w-96 text-justify">
+          We are creating a newsletter about tech topics to keep you updated on
+          the latest news and updates.
+        </p>
+      </section>
+    </main>
+  );
+}
